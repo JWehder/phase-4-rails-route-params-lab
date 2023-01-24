194ec468a4fad6448360483c35deece3072db3d2
Jake Wehder <jw@email.comjake>
2023-01-24 13:56:18 -0600

    commit tree 0557edd9644811cde926479904ca69d183c4f4ee

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,13 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.find_by(first_name: params[:name] || last_name: params[:name])
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
